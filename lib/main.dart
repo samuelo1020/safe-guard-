d707d3d0eb567cdcafc8b97fdc8a7fce934c6312
dannasofi06 <danna.sofia.salcedo@gmail.com>
2024-03-23 23:53:24 -0500

    se formateo

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,21 @@ import 'package:flutter/material.dart';
 
 import 'pages/login_page.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-@override  
   @override
-  Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: ' Login App',
       initialRoute: '/login',
-      routes:{
-        '/login':(context) => const LoginPage(),
-      
+      routes: {
+        '/login': (context) => const LoginPage(),
       },
     );
   }
